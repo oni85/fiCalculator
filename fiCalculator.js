15b94ad93918809d68459e7450758856c70d53b0
oni85 <o.natalchenko@gmail.com>
2018-02-22 23:39:58 +0200

    js start

--- fiCalculator.js
+++ fiCalculator.js
@@ -1,9 +1,15 @@
 //------------INPUT VALIDATION-------------//
-function inputValidation(input) {
+/*function inputValidation(input) {
   let value  = input.value;
   let rep = /[-\.;":'/a-zA-Zа-яА-Я ]/;
   if (rep.test(value)) {
     value = value.replace(rep, '');
     input.value = value;
   }
+}*/
+function cislo(){
+    if (event.keyCode < 48 || event.keyCode > 57)
+    event.returnValue= false;
 }
+let a = [document.querySelector('.tableRy').textContent];
+console.log(a);
