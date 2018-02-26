226a134c14b317d91406c96449f475305e33db88
oni85 <o.natalchenko@gmail.com>
2018-02-26 18:51:36 +0200

    initial commit

--- fiCalculator.js
+++ fiCalculator.js
@@ -17,16 +17,34 @@ console.log(a);
 //----------CLICK ON CALCULATE BUTTON-----------------------//
 
 document.getElementById('calcBtn').addEventListener('click', function calculate() {
+  //----------CREATING VARIABLES FROM INPUT DATA-----------------------//
   let rY = document.getElementById('rY').value;
   let lambda = document.getElementById('lambda').value;
-  console.log(rY);
-  console.log(lambda);
-  let rYarr = [];
-
-  console.log(document.getElementById('tableRy').children[0].children[0]);
+  let ryArr = [];
+  let lambdaArr = [];
+  let fiArr = [];
+  let fiArrContent = [];
+  //console.log(document.getElementById('tableRy').children[0].children[0]);
   for (var i = 0; i < (document.getElementById('tableRy').children[0].children[0].children).length; i++) {
-    rYarr[i] = document.getElementById('tableRy').children[0].children[0].children[i].textContent;
+    ryArr[i] = document.getElementById('tableRy').children[0].children[0].children[i].textContent;
   }
-  console.log(rYarr);
+  console.log(ryArr);
+
+  //console.log(document.getElementById('tableLambda').children[0].children[0]);
+  for (var i = 0; i < document.getElementById('tableLambda').children[0].children.length; i++) {
+    lambdaArr[i] = document.getElementById('tableLambda').children[0].children[i].children[0].textContent;
+  }
+  console.log(lambdaArr);
+  //console.log(document.getElementById('tableFi').children[0].children[0].children[0].textContent);
+  for (var i = 0; i < document.getElementById('tableFi').children[0].children.length; i++) {
+        for (var y = 0; y < document.getElementById('tableFi').children[0].children[0].children.length; y++) {
+        fiArrContent[y] = document.getElementById('tableFi').children[0].children[0].children[y].textContent;
+        console.log(fiArrContent);
+        //fiArr[i] = [fiArrContent];
+      }
+      fiArr[i] = [fiArrContent];
+  }
+  console.log(fiArr);
+
 }
 )
