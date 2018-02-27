5da236ba19dc7070a4c84275917a541eb753c155
oni85 <o.natalchenko@gmail.com>
2018-02-27 17:54:42 +0200

    initial commit

--- fiCalculator.js
+++ fiCalculator.js
@@ -1,5 +1,5 @@
-//------------INPUT VALIDATION-------------//
-/*function inputValidation(input) {
+// ------------INPUT VALIDATION-------------//
+/* function inputValidation(input) {
   let value  = input.value;
   let rep = /[-\.;":'/a-zA-Zа-яА-Я ]/;
   if (rep.test(value)) {
@@ -14,30 +14,28 @@ function cislo(){
 let a = [document.querySelector('.tableRy').textContent];
 console.log(a);
 */
-//----------CLICK ON CALCULATE BUTTON-----------------------//
-
-//----------INPUT VALIDATION-----------------------//
+// ----------CLICK ON CALCULATE BUTTON-----------------------//
 
 document.getElementById('calcBtn').addEventListener('click', function calculate() {
-  //----------CREATING VARIABLES FROM INPUT DATA-----------------------//
+  // ----------CREATING VARIABLES FROM INPUT DATA-----------------------//
   let rY = document.getElementById('rY').value;
   let lambda = document.getElementById('lambda').value;
   let ryArr = [];
   let lambdaArr = [];
   let fiArr = [];
 
-  //console.log(document.getElementById('tableRy').children[0].children[0]);
+  // console.log(document.getElementById('tableRy').children[0].children[0]);
   for (var i = 0; i < (document.getElementById('tableRy').children[0].children[0].children).length; i++) {
     ryArr[i] = document.getElementById('tableRy').children[0].children[0].children[i].textContent;
   }
-//  console.log(ryArr);
+  console.log(ryArr);
 
-  //console.log(document.getElementById('tableLambda').children[0].children[0]);
+  // console.log(document.getElementById('tableLambda').children[0].children[0]);
   for (var i = 0; i < document.getElementById('tableLambda').children[0].children.length; i++) {
     lambdaArr[i] = document.getElementById('tableLambda').children[0].children[i].children[0].textContent;
   }
-  //console.log(lambdaArr);
-  //console.log(document.getElementById('tableFi').children[0].children[0].children[0].textContent);
+  console.log(lambdaArr);
+  // console.log(document.getElementById('tableFi').children[0].children[0].children[0].textContent);
   for (var i = 0; i < document.getElementById('tableFi').children[0].children.length; i++) {
         let fiArrContent = [];
         for (var y = 0; y < document.getElementById('tableFi').children[0].children[i].children.length; y++) {
@@ -45,8 +43,49 @@ document.getElementById('calcBtn').addEventListener('click', function calculate(
         //console.log(fiArrContent);
       }
       fiArr[i] = fiArrContent;
-      //console.log(fiArr);
   }
+//console.log(fiArr);
+// ----------INPUT VALIDATION----------------------- //
+  if (isNaN (rY)) {
+    document.getElementById('ryValidation').classList.add('displayBlock', 'transitionDiv');
+    document.getElementById('ryValidation').innerHTML = '<p class="centered">Only numbers allowed</p>';
+  }
+  if (isNaN (lambda)) {
+    document.getElementById('lambdaValidation').classList.add('displayBlock', 'transitionDiv');
+    document.getElementById('lambdaValidation').innerHTML = '<p class="centered">Only numbers allowed</p>';
+  }
+  if (rY < 200) {
+    document.getElementById('ryValidation').classList.add('displayBlock', 'transitionDiv');
+    document.getElementById('ryValidation').innerHTML = '<p class="centered">Ry is less than 200 mPa</p>';
+  }
+  if (rY > 640) {
+    document.getElementById('ryValidation').classList.add('displayBlock', 'transitionDiv');
+    document.getElementById('ryValidation').innerHTML = '<p class="centered">Ry is more than 640 mPa</p>';
+  }
+  if (rY === '') {
+    document.getElementById('ryValidation').classList.add('displayBlock', 'transitionDiv');
+    document.getElementById('ryValidation').innerHTML = '<p class="centered">Fill this field</p>';
+  }
+  if (lambda < 10) {
+    document.getElementById('lambdaValidation').classList.add('displayBlock', 'transitionDiv');
+    document.getElementById('lambdaValidation').innerHTML = '<p class="centered">&#955; is less than 10</p>';
+  }
+  if (lambda > 220) {
+    document.getElementById('lambdaValidation').classList.add('displayBlock', 'transitionDiv');
+    document.getElementById('lambdaValidation').innerHTML = '<p class="centered">&#955; is more than 220</p>';
+  }
+  if (lambda === '') {
+    document.getElementById('lambdaValidation').classList.add('displayBlock', 'transitionDiv');
+    document.getElementById('lambdaValidation').innerHTML = '<p class="centered">Fill this field</p>';
+  }
+  let a;
+  let b;
 
+  while (var i = 0; i < ryArr.length; i++) {
+    if (rY < ryArr[i]) {
+      a = ryArr[i];
+    } else a = 'none';
+  }
+  console.log(a);
 }
 )
