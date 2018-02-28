fef24607a681a528838f2640a9e4cce6bacd70d2
oni85 <o.natalchenko@gmail.com>
2018-02-28 19:04:13 +0200

    initial commit

--- fiCalculator.js
+++ fiCalculator.js
@@ -1,8 +1,15 @@
-
+  // ----------PROGRAMM INIT FUNCTION ----------------------- //
+function programmInit() {
+  document.getElementById('tableRy').children[0].children[0].classList.remove('tdActive')
+  //document.getElementById('tableLambda').children.classList.remove('tdActive');
+}
+  // ----------CLICK ON CALCULATE BUTTON ----------------------- //
 document.getElementById('calcBtn').addEventListener('click', function calculate() {
-  // ----------CREATING VARIABLES FROM INPUT DATA----------------------- //
+  programmInit();
+  // ----------CREATING VARIABLES FROM INPUT DATA ----------------------- //
   let rY = document.getElementById('rY').value;
-  let lambda = document.getElementById('lambda').value;
+  let lambdaC = document.getElementById('lambda').value;
+  console.log(typeof lambdaC);
   let ryArr = [];
   let lambdaArr = [];
   let fiArr = [];
@@ -17,12 +24,13 @@ document.getElementById('calcBtn').addEventListener('click', function calculate(
   console.log(ryArr);
 
   // console.log(document.getElementById('tableLambda').children[0].children[0]);
-  for (var i = 0; i < document.getElementById('tableLambda').children[0].children.length; i++) {
+  for (i = 0; i < document.getElementById('tableLambda').children[0].children.length; i++) {
     lambdaArr[i] = document.getElementById('tableLambda').children[0].children[i].children[0].textContent;
   }
   console.log(lambdaArr);
+  console.log(lambdaArr.length);
   // console.log(document.getElementById('tableFi').children[0].children[0].children[0].textContent);
-  for (var i = 0; i < document.getElementById('tableFi').children[0].children.length; i++) {
+  for (i = 0; i < document.getElementById('tableFi').children[0].children.length; i++) {
         let fiArrContent = [];
         for (var y = 0; y < document.getElementById('tableFi').children[0].children[i].children.length; y++) {
         fiArrContent[y] = document.getElementById('tableFi').children[0].children[i].children[y].textContent;
@@ -31,7 +39,7 @@ document.getElementById('calcBtn').addEventListener('click', function calculate(
       fiArr[i] = fiArrContent;
   }
 //console.log(fiArr);
-// ----------INPUT VALIDATION----------------------- //
+  // ----------INPUT VALIDATION----------------------- //
   if (isNaN (rY)) {
     document.getElementById('ryValidation').classList.add('displayBlock', 'transitionDiv');
     document.getElementById('ryValidation').innerHTML = '<p class="centered">Only numbers allowed</p>';
@@ -64,14 +72,15 @@ document.getElementById('calcBtn').addEventListener('click', function calculate(
     document.getElementById('lambdaValidation').classList.add('displayBlock', 'transitionDiv');
     document.getElementById('lambdaValidation').innerHTML = '<p class="centered">Fill this field</p>';
   }
-// ----------RYINDEX ARRAY FORMING----------------------- //
-  for (var i = 0; i < ryArr.length; i++) {
-      if (rY === ryArr[i]) {
+  // ----------RYINDEX ARRAY FORMING----------------------- //
 
+  for (let i = 0; i < ryArr.length; i++) {
+      if (rY === ryArr[i]) {
         let c = ryArr.indexOf(ryArr[i]);
         ryIndex.push(c);
         document.getElementById('tableRy').children[0].children[0].children[i].classList.add('tdActive');
         console.log(ryIndex);
+        //console.log(i);
         break;
       } else if (rY < ryArr[i]) {
         let a = ryArr.indexOf(ryArr[i - 1]);
@@ -80,34 +89,42 @@ document.getElementById('calcBtn').addEventListener('click', function calculate(
         console.log(ryIndex);
         document.getElementById('tableRy').children[0].children[0].children[i - 1].classList.add('tdActive');
         document.getElementById('tableRy').children[0].children[0].children[i].classList.add('tdActive');
+        //console.log(i);
         break;
       } else if (rY > ryArr[i]) {
-
+        //console.log(i);
       }
     }
-// ----------LAMBDAINDEX ARRAY FORMING----------------------- //
-for (var i = 0; i < lambdaArr.length; i++) {
-    if (lambda === lambdaArr[i]) {
-
-      let f = lambdaArr.indexOf(lambdaArr[i]);
-      lambdaIndex.push(f);
-      document.getElementById('tableLambda').children[0].children[i].children[0].classList.add('tdActive');
-      console.log(lambdaIndex);
+    //console.log(i);
+  // ----------LAMBDAINDEX ARRAY FORMING----------------------- //
+  //console.log('lambda = ' + lambda);
+  /*for (let i = 0; i < lambdaArr.length; i++) {
+    console.log(lambdaArr[i]);
+  }*/
+  for (let i = 0; i < lambdaArr.length; i++) {
+    if (lambdaC === lambdaArr[i]) {
+        lambdaIndex.push(lambdaArr.indexOf(lambdaArr[i]));
+        console.log('i = ' + i);
+        console.log('lambdaC = ' + lambdaC);
+        console.log('lambdaArr[i] = ' + lambdaArr[i]);
+        break;
+    } else if (+lambdaC < +lambdaArr[i]) {
+        lambdaIndex.push((lambdaArr.indexOf(lambdaArr[i - 1])), (lambdaArr.indexOf(lambdaArr[i])));
+        console.log('i = ' + i);
+        console.log('lambdaC = ' + lambdaC);
+        console.log('lambdaArr[i] = ' + lambdaArr[i]);
       break;
-    } else if (lambda < lambdaArr[i]) {
-      let d = lambdaArr.indexOf(lambdaArr[i - 1]);
-      let e = lambdaArr.indexOf(lambdaArr[i]);
-      lambdaIndex.push(d, e);
-      console.log(lambdaIndex);
-      document.getElementById('tableLambda').children[0].children[i - 1].children[0].classList.add('tdActive');
-      document.getElementById('tableLambda').children[0].children[i].children[0].classList.add('tdActive');
-      break;
-    } else if (lambda > lambdaArr[i]) {
-
+    } else if (lambdaC > lambdaArr[i]) {
+        console.log('i = ' + i);
+        console.log('lambdaC = ' + lambdaC);
+        console.log('lambdaArr[i] = ' + lambdaArr[i]);
     }
   }
-// ----------FIINDEX ARRAY FORMING----------------------- //
-fiIndex.push(ryIndex, lambdaIndex);
-console.log(fiIndex);
+  console.log(lambdaIndex);
+  // ----------FIINDEX ARRAY FORMING----------------------- //
+  fiIndex.push(ryIndex, lambdaIndex);
+  console.log(fiIndex);
+//  console.log(ryArr.length);
+//  console.log(lambdaArr.length);
 }
 )
