f337e7d67d8fc6cd04c07b88d60195fa972af7b1
oni85 <o.natalchenko@gmail.com>
2018-03-03 23:15:54 +0200

    initial commit

--- fiCalculator.js
+++ fiCalculator.js
@@ -1,7 +1,10 @@
 // ----------PROGRAMM INIT FUNCTION ----------------------- //
+
 function programmInit () {
   let i = 0;
-  // ----------CLEANING TABLES FROM MARKS ----------------------- //
+
+// cleaning tables from marks
+
   for (i = 0; i < (document.getElementById('tableRy').children[0].children[0].children).length; i++) {
     document.getElementById('tableRy').children[0].children[0].children[i].classList.remove('tdActive');
   }
@@ -13,17 +16,24 @@ function programmInit () {
         document.getElementById('tableFi').children[0].children[i].children[y].classList.remove('tdActive');
       }
   }
-  // ----------CLEANING VALIDATION MESSAGES ----------------------- //
+// ----------INTERPOLATION FUNCTION----------------------- //
+
+  function interpolator(x, x1, x2, y1, y2) {
+    intRes = y1 + (x - x1) * ((y2 - y1) / (x2 - x1));
+    return intRes;
+  }
+// ----------CLEANING VALIDATION MESSAGES ----------------------- //
+
   document.getElementById('lambdaValidation').classList.remove('displayBlock');
   document.getElementById('ryValidation').classList.remove('displayBlock');
 }
-  // ----------CLICK ON CALCULATE BUTTON ----------------------- //
+// ----------CLICK ON CALCULATE BUTTON ----------------------- //
 
 document.getElementById('calcBtn').addEventListener('click', function calculate() {
 
   programmInit();
 
-  // ----------CREATING VARIABLES FROM INPUT DATA ----------------------- //
+// ----------CREATING VARIABLES FROM INPUT DATA ----------------------- //
 
   let rY = parseInt(document.getElementById('rY').value);
   let lambdaC = parseInt(document.getElementById('lambda').value);
@@ -33,6 +43,10 @@ document.getElementById('calcBtn').addEventListener('click', function calculate(
   let ryIndex = [];
   let lambdaIndex = [];
   let fiIndex = [];
+  let y1;
+  let y2;
+  let y3;
+  let y4;
 
   for (var i = 0; i < (document.getElementById('tableRy').children[0].children[0].children).length; i++) {
     ryArr[i] = document.getElementById('tableRy').children[0].children[0].children[i].textContent;
@@ -46,12 +60,11 @@ document.getElementById('calcBtn').addEventListener('click', function calculate(
         let fiArrContent = [];
         for (var y = 0; y < document.getElementById('tableFi').children[0].children[i].children.length; y++) {
         fiArrContent[y] = document.getElementById('tableFi').children[0].children[i].children[y].textContent;
-        //console.log(fiArrContent);
       }
       fiArr[i] = fiArrContent;
   }
 
-  // ----------INPUT VALIDATION----------------------- //
+// ----------INPUT VALIDATION----------------------- //
   if (isNaN (rY)) {
     document.getElementById('ryValidation').classList.add('displayBlock', 'transitionDiv');
     document.getElementById('ryValidation').innerHTML = '<p class="centered">Only numbers allowed</p>';
@@ -84,7 +97,7 @@ document.getElementById('calcBtn').addEventListener('click', function calculate(
     document.getElementById('lambdaValidation').classList.add('displayBlock', 'transitionDiv');
     document.getElementById('lambdaValidation').innerHTML = '<p class="centered">Fill this field</p>';
   }
-  // ----------RYINDEX ARRAY FORMING----------------------- //
+// ----------RYINDEX ARRAY FORMING----------------------- //
   for (let i = 0; i < ryArr.length; i++) {
       if (rY === +ryArr[i]) {
         let c = ryArr.indexOf(ryArr[i]);
@@ -97,6 +110,8 @@ document.getElementById('calcBtn').addEventListener('click', function calculate(
       } else if (rY < +ryArr[i]) {
         let a = ryArr.indexOf(ryArr[i - 1]);
         let b = ryArr.indexOf(ryArr[i]);
+        y1 = ryArr[i - 1];
+        y2 = ryArr[i];
         ryIndex.push(a, b);
         //console.log(ryIndex);
         // RYTABLE CELLS HIGHLIGHTING
@@ -109,7 +124,7 @@ document.getElementById('calcBtn').addEventListener('click', function calculate(
       }
     }
     //console.log(i);
-  // ----------LAMBDAINDEX ARRAY FORMING----------------------- //
+// ----------LAMBDAINDEX ARRAY FORMING----------------------- //
   for (let i = 0; i < lambdaArr.length; i++) {
     if (lambdaC === +lambdaArr[i]) {
         lambdaIndex.push(lambdaArr.indexOf(lambdaArr[i]));
@@ -118,6 +133,8 @@ document.getElementById('calcBtn').addEventListener('click', function calculate(
         break;
     } else if (lambdaC < +lambdaArr[i]) {
         lambdaIndex.push((lambdaArr.indexOf(lambdaArr[i - 1])), (lambdaArr.indexOf(lambdaArr[i])));
+        y3 = lambdaArr[i - 1];
+        y4 = lambdaArr[i];
         // LAMBDATABLE CELLS HIGHLIGHTING
         document.getElementById('tableLambda').children[0].children[i - 1].children[0].classList.add('tdActive');
         document.getElementById('tableLambda').children[0].children[i].children[0].classList.add('tdActive');
@@ -126,16 +143,31 @@ document.getElementById('calcBtn').addEventListener('click', function calculate(
 
     }
   }
-  // ----------FIINDEX ARRAY FORMING----------------------- //
+// ----------FIINDEX ARRAY FORMING----------------------- //
 
   fiIndex.push(ryIndex, lambdaIndex);
 
-  // ----------FITABLE CELLS HIGHLIGHTING----------------------- //
+// ----------FITABLE CELLS HIGHLIGHTING----------------------- //
   if (fiIndex[0].length === 2 && fiIndex[1].length === 2) {
+    //table marks add
     document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][0]].classList.add('tdActive');
     document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][1]].classList.add('tdActive');
     document.getElementById('tableFi').children[0].children[fiIndex[1][1]].children[fiIndex[0][0]].classList.add('tdActive');
     document.getElementById('tableFi').children[0].children[fiIndex[1][1]].children[fiIndex[0][1]].classList.add('tdActive');
+    // fi calculate
+    let x1 = document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][0]].textContent;
+    console.log('x1 = ' + x1);
+    let x2 = document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][1]].textContent;
+    console.log('x2 = ' + x2);
+    let x3 = document.getElementById('tableFi').children[0].children[fiIndex[1][1]].children[fiIndex[0][0]].textContent;
+    console.log('x3 = ' + x3);
+    let x4 = document.getElementById('tableFi').children[0].children[fiIndex[1][1]].children[fiIndex[0][1]].textContent;
+    console.log('x4 = ' + x4);
+    console.log('y1 = ' + y1);
+    console.log('y2 = ' + y2);
+    console.log('y3 = ' + y3);
+    console.log('y4 = ' + y4);
+
   } else if (fiIndex[0].length === 2 && fiIndex[1].length === 1) {
     document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][0]].classList.add('tdActive');
     document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][1]].classList.add('tdActive');
@@ -145,11 +177,6 @@ document.getElementById('calcBtn').addEventListener('click', function calculate(
   } else {
     document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][0]].classList.add('tdActive');
   }
-  // ----------INTERPOLATION FUNCTION----------------------- //
-  function interpolator(x, x1, x2, y1, y2) {
-    intRes = y1 + (x - x1) * ((y2 - y1) / (x2 - x1));
-    return intRes;
-  }
-  console.log(interpolator(15, 10, 30, 50, 70));
+
 }
 )
