28c0848489a8ec71de660ce1b1092fad68854609
oni85 <o.natalchenko@gmail.com>
2018-03-07 22:07:02 +0200

    initial commit

--- fiCalculator.js
+++ fiCalculator.js
@@ -89,7 +89,6 @@ function dataValidation (p1, p2) {
     validationArr.push(isValid);
   }
   if (document.getElementById('rY').value.length === 0) {
-    // console.log(document.getElementById('rY').value.length);
     document.getElementById('ryValidation').classList.add('displayBlock', 'transitionDiv');
     document.getElementById('ryValidation').innerHTML = '<p class="centered">Fill this field</p>';
     isValid = false;
@@ -178,7 +177,6 @@ document.getElementById('calcBtn').addEventListener('click', function calculate 
         // cells highlighting
         document.getElementById('tableRy').children[0].children[0].children[i].classList.add('tdActive');
         x1 = parseInt(ryArr[i]);
-        console.log('x1 = ' + x1);
         break;
       } else if (rY < +ryArr[i]) {
         x1 = parseInt(ryArr[i - 1]);
@@ -193,6 +191,7 @@ document.getElementById('calcBtn').addEventListener('click', function calculate 
     }
 
     // lambda index array forming
+
     for (let i = 0; i < lambdaArr.length; i++) {
       if (lambdaC === +lambdaArr[i]) {
         lambdaIndex.push(lambdaArr.indexOf(lambdaArr[i]));
@@ -204,9 +203,7 @@ document.getElementById('calcBtn').addEventListener('click', function calculate 
       } else if (lambdaC < +lambdaArr[i]) {
         lambdaIndex.push((lambdaArr.indexOf(lambdaArr[i - 1])), (lambdaArr.indexOf(lambdaArr[i])));
         x3 = parseInt(lambdaArr[i - 1]);
-        // console.log('x3 = ' + x3);
         x4 = parseInt(lambdaArr[i]);
-        // console.log('x4 = ' + x4);
         // cells highlighting
         document.getElementById('tableLambda').children[0].children[i - 1].children[0].classList.add('tdActive');
         document.getElementById('tableLambda').children[0].children[i].children[0].classList.add('tdActive');
@@ -228,26 +225,12 @@ document.getElementById('calcBtn').addEventListener('click', function calculate 
       document.getElementById('tableFi').children[0].children[fiIndex[1][1]].children[fiIndex[0][1]].classList.add('tdActive');
       // fi calculate
       let y1 = parseInt(document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][0]].textContent);
-      // console.log('x1 = ' + x1);
       let y2 = parseInt(document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][1]].textContent);
-      // console.log('x2 = ' + x2);
       let y3 = parseInt(document.getElementById('tableFi').children[0].children[fiIndex[1][1]].children[fiIndex[0][0]].textContent);
-      // console.log('x3 = ' + x3);
       let y4 = parseInt(document.getElementById('tableFi').children[0].children[fiIndex[1][1]].children[fiIndex[0][1]].textContent);
-      // console.log('x4 = ' + x4);
-      // console.log('y1 = ' + y1);
-      // console.log('y2 = ' + y2);
-      // console.log('y3 = ' + y3);
-      // console.log('y4 = ' + y4);
       fi1 = interpolator(rY, x1, x2, y1, y2);
-      // console.log(fi1);
-      // console.log(typeof fi1);
       fi2 = interpolator(rY, x1, x2, y3, y4);
-      // console.log(fi2);
-      // console.log(typeof fi2);
       fi = interpolator(lambdaC, x3, x4, fi1, fi2);
-      // console.log(fi);
-      // console.log(typeof fi);
       document.getElementById('fI').textContent = (fi / 1000);
     } else if (fiIndex[0].length === 2 && fiIndex[1].length === 1) {
       // table marks add
@@ -255,25 +238,17 @@ document.getElementById('calcBtn').addEventListener('click', function calculate 
       document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][1]].classList.add('tdActive');
       // fi calculate
       let y1 = parseInt(document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][0]].textContent);
-      // console.log('y1 = ' + x1);
       let y2 = parseInt(document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][1]].textContent);
-      // console.log('y2 = ' + x2);
       fi = interpolator(rY, x1, x2, y1, y2);
-      // console.log(fi);
-      // console.log(typeof fi);
       document.getElementById('fI').textContent = (fi / 1000);
     } else if (fiIndex[0].length === 1 && fiIndex[1].length === 2) {
       // table marks add
       document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][0]].classList.add('tdActive');
       document.getElementById('tableFi').children[0].children[fiIndex[1][1]].children[fiIndex[0][0]].classList.add('tdActive');
       let y1 = parseInt(document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][0]].textContent);
-      // console.log('y1 = ' + y1);
       let y3 = parseInt(document.getElementById('tableFi').children[0].children[fiIndex[1][1]].children[fiIndex[0][0]].textContent);
-      // console.log('y3 = ' + y3);
       // fi calculate
       fi = interpolator(lambdaC, x3, x4, y1, y3);
-      // console.log(fi);
-      // console.log(typeof fi);
       document.getElementById('fI').textContent = (fi / 1000);
     } else {
       document.getElementById('tableFi').children[0].children[fiIndex[1][0]].children[fiIndex[0][0]].classList.add('tdActive');
